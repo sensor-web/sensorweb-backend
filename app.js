544e7b8d752fd863b50d8dc7a5d522a8cf25569a
Evan Xd <itoyxd@gmail.com>
2016-03-24 00:41:44 +0800

    Implement getSensors method

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 'use strict';
 
 var express = require('express');
+var mongoose = require('mongoose');
+
 var users = require('./lib/users');
 var sensors = require('./lib/sensors');
 var projects = require('./lib/projects');
 var config = require('./config.js');
 var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  app.route('/:userId/sensors')
+    .get(sensors.getSensors);
+
+  app.route('/sensors')
+    .get(sensors.getSensors)
+    .post(sensors.addSensor);
+
+  app.route('/sensors/:sensorId')
+    .get(sensors.getSensors);
+
+  app.route('/sensors/:id/data')
+    .get(sensors.getSensorData)
+    .post(sensors.addSensorData);
+});
 
 // The below routers are for the webpages.
 app.use(express.static('./public'));
@@ -14,19 +36,6 @@ app.route('/:userId');
 // The below routers are for the REST APIs.
 app.route('/users')
   .post(users.addUser);
-
-app.route('/:userId/sensors')
-  .get(sensors.getSensors);
-
-app.route('/sensors')
-  .post(sensors.addSensor);
-
-app.route('/sensors/:sensorId')
-  .get(sensors.getSensors);
-
-app.route('/sensors/:id/data')
-  .get(sensors.getSensorData)
-  .post(sensors.addSensorData);
 
 app.route('/projects')
   .get(projects.getProjects)
