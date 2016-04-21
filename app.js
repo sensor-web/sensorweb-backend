21cf4eb7570022074d300553d5612f23b449a1ac
Evan Xd <itoyxd@gmail.com>
2016-04-21 18:42:12 +0800

    Remove useless header

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods: POST');
   res.header('Access-Control-Max-Age: 1000');
-  // res.header('Access-Control-Allow-Headers',
-  //   'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
