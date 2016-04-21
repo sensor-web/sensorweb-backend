c5b8270f7a6414922b960b1aab3b6d78180e718d
Evan Xd <itoyxd@gmail.com>
2016-04-21 16:34:59 +0800

    Enable CORS POST

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ config.dev && app.use(express.static('./sensorweb-frontend'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods: POST');
+  res.header('Access-Control-Max-Age: 1000');
+  // res.header('Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
