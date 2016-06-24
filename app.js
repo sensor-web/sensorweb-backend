77961cb7e17bb76e2f22c01d39fff8c6e4fc4913
Evan Tseng <evan@tseng.io>
2016-06-24 18:20:03 +0800

    Bug 1282033 - Add comments for REST API routers

--- app.js
+++ app.js
@@ -32,36 +32,45 @@ db.once('open', function() {
     });
   });
 
+  // Get, update and delete a sensor information.
   app.route('/sensors/:sensorId')
     .get(sensors.getSensors)
     .put(sensors.editSensor)
     .delete(sensors.deleteSensor);
 
+  // Get a sensor's information and data.
   app.route('/sensors/:sensorId/data')
     .get(sensors.getSensorData)
     .post(sensors.addSensorData);
 
+  // Get a project information.
   app.route('/projects/:userId/:projectName')
     .get(projects.getProjects);
 
+  // Get project's sensor list and add a sensor for a project.
   app.route('/projects/:userId/:projectName/sensors')
     .get(sensors.getSensors)
     .post(sensors.addSensor);
 
+  // Get project's contributor list.
   app.route('/projects/:userId/:projectName/contributors')
     .get(projects.getContributors);
 
+  // Get user information by email and add a user.
   app.route('/users')
     // Get user data by email: `/users?email=[USER_EMAIL]`.
     .get(users.getUserData)
     .post(users.addUser);
 
+  // Get user information by user ID.
   app.route('/users/:userId')
     .get(users.getUserData);
 
+  // Get user's sensor list.
   app.route('/users/:userId/sensors')
     .get(sensors.getSensors);
 
+  // Get user's project list.
   app.route('/users/:userId/projects')
     .get(projects.getProjects);
 });
